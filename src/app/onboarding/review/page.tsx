7f4f1ae652df4a4c42f4c5c68ee339e0475f29b2
doofx0071 <pplona746@gmail.com>
2025-10-14 05:51:08 +0800

    Fix button sizing consistency in onboarding review page - make Back and Create My Profile buttons same size

--- src/app/onboarding/review/page.tsx
+++ src/app/onboarding/review/page.tsx
@@ -295,7 +295,7 @@ export default function ReviewPage() {
 
         {/* Submit Button */}
         <div className="flex flex-col sm:flex-row gap-4 pt-4">
-          <Button type="button" variant="outline" className="flex-1 cursor-pointer" asChild>
+          <Button type="button" variant="outline" className="flex-1 cursor-pointer" size="lg" asChild>
             <Link href="/onboarding/dietary">Back</Link>
           </Button>
           <Button
